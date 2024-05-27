71243e32f678e465c9086b51a5226e33a4df3b9a
Yannick Mueller <info@yannickmueller.dev>
2024-05-27 21:58:15 +0200

    began to make home and added some images

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.getElementById('menu-icon');
-    const navLinks = document.getElementById('nav');
+function init() {
 
-    menuIcon.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
+    const ymqsc = document.getElementById('ymqscph');
+    const pre = document.getElementById('prehome');
+    ymqsc.style.height = pre.offsetHeight + 'px';
+
+    document.getElementById('menu-toggle').addEventListener('change', function() {
+        const nav = document.getElementById('navi');
+        if (this.checked) {
+            nav.style.backgroundColor = 'var(--bg)';  // Change as needed
+        } else {
+            nav.style.backgroundColor = 'rgba(255,255,255,0.3)'; // Reset to initial or default
+        }
     });
-});
+};
